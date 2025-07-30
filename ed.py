5f5269739eb81c5b26f24e0a4cd8b0ffd16e36ce
roybin1212 <roibin.efrain@gmail.com>
2025-07-30 18:25:11 +0000

    ver

---
+++ ed.py
@@ -0,0 +1,10 @@
+while True:
+    
+    num = int(input("ingresa un numero"))
+
+    if num == 10:
+        print("hola mundo")
+        break
+    
+    else: 
+        print("el numero es incorrecto")
